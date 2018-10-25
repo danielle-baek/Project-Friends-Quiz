e34360c17f7d8ff2429f6601312f17e7b410f5e2
Jeremy Jonas <jeremy.jonas@hotmail.co.nz>
2018-10-26 10:50:02 +1300

    started base html

--- routes.js
+++ routes.js
@@ -2,25 +2,49 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    console.log('/friends/ route hit')
+  console.log('/friends/ route hit')
   res.redirect('/friends/home')
 })
 
 router.get('/home', (req, res) => {
-    console.log('home route hit')
+  console.log('home route hit')
   res.send('This is home')
 })
 
 router.get('/results', (req, res) => {
-    console.log('results route hit')
+  console.log('results route hit')
   res.send('This is results')
+})
+
+router.get('/test', (req, res) => {
+  console.log('testing')
+  res.render('./questions.hbs')
+})
+
+// router.get('/test', (req, res) => {
+//   console.log('testing')
+//   res.render('./index.hbs')
+// })
+
+router.get('/test', (req, res) => {
+  console.log('testing')
+  res.render('./result.hbs')
 })
 
 router.get('/:id', (req, res) => {
   let id = req.params.id
-    console.log('questions route hit')
-    console.log(id)
+  console.log('questions route hit')
+  console.log(id)
   res.send('This is a question page')
 })
 
 module.exports = router
+
+
+
+
+
+
+
+
+
