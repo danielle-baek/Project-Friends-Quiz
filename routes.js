60cf9cec3c8c39820c8fa386f6902c85f9befe73
Dasol Baek <schgana@hotmail.com>
2018-10-26 10:56:34 +1300

    linter fix

--- routes.js
+++ routes.js
@@ -3,25 +3,25 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    console.log('/friends/ route hit')
+  console.log('/friends/ route hit')
   res.redirect('/friends/home')
 })
 
 router.get('/home', (req, res) => {
-    console.log('home route hit')
+  console.log('home route hit')
   res.send('This is home')
 })
 
 router.get('/results', (req, res) => {
-    console.log('results route hit')
+  console.log('results route hit')
   res.send('This is results')
 })
 
 router.get('/:id', (req, res) => {
   let id = req.params.id
   let ques = data.Questions[id - 1]
-    console.log('questions route hit')
-    console.log(ques)
+  console.log('questions route hit')
+  console.log(ques)
   res.render('./views/questions')
 })
 
