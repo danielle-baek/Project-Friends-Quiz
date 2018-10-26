506f52dfd4e8360015f10992e7dea25e6ce13871
danielle-baek <schgana@hotmail.com>
2018-10-26 14:19:20 +1300

    working on getting data

--- public/script.js
+++ public/script.js
@@ -1,10 +1,15 @@
-function proceed(n) {
+const data = require('../questions.json')
+
+function proceed(quesNum, ansNum) {
   var form = document.createElement('form')
-  console.log(n)
   form.setAttribute('method', 'post')
-  form.setAttribute('action', '/friends/' + (n+1))
+  form.setAttribute('action', '/friends/' + (quesNum+1))
   form.style.display = 'hidden'
   document.body.appendChild(form)
   form.submit()
+}
 
+function getCharacter (qNum, aNum) {
+  let char = 'C' + aNum
+  data.Questions[qNum - 1].char
 }
