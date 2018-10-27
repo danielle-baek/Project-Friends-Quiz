5181cd3ee00540e6f842c38b769c80c4ac5d6901
CarelS072 <carelstarreveld@hotmail.com>
2018-10-27 13:42:34 +1300

    removed requirement for local script, changed routes, added test Questions HBS view for testing

--- public/script.js
+++ public/script.js
@@ -1,17 +1,17 @@
 //const data = require('../questions.json')
 // const fs = require('fs')
 
-function proceed (quesNum, ansNum) {
-  // fs.writeFile('./answers.json', JSON.stringify(document.getElementById('value').innerHTML), 'utf8', function (err) {
-  //   if (err) {
-  //     // eslint-disable-next-line no-console
-  //     return console.log(err)
-  //   }
-  // })
-  var form = document.createElement('form')
-  form.setAttribute('method', 'post')
-  form.setAttribute('action', '/friends/' + (quesNum + 1))
-  form.style.display = 'hidden'
-  document.body.appendChild(form)
-  form.submit()
-}
+// function proceed (quesNum, ansNum) {
+//   // fs.writeFile('./answers.json', JSON.stringify(document.getElementById('value').innerHTML), 'utf8', function (err) {
+//   //   if (err) {
+//   //     // eslint-disable-next-line no-console
+//   //     return console.log(err)
+//   //   }
+//   // })
+//   var form = document.createElement('form')
+//   form.setAttribute('method', 'post')
+//   form.setAttribute('action', '/friends/' + (quesNum))
+//   form.style.display = 'hidden'
+//   document.body.appendChild(form)
+//   form.submit()
+// }
--- routes.js
+++ routes.js
@@ -33,38 +33,68 @@ router.post('/start', (req, res) => {
   res.redirect('/friends/start')
 })
 
+//changed start to redirect to page 1 rather then rendering 1st Question
 router.get('/start', (req, res) => {
+  console.log('start route hit')
+  res.redirect('/friends/1')
+})
+router.get('/1', (req, res) => {
   let id = req.params.id
   let ques = data.Questions[0]
-  console.log('questions route hit')
-  console.log(ques)
-  res.render('./questions.hbs', ques)
-})
-
-router.get('/:id', (req, res) => {
-  let id = req.params.id
-  let ques = data.Questions[id - 1]
-  console.log('questions route hit')
-  console.log(ques)
-  res.render('./questions.hbs', ques)
+  console.log('1st Question route hit')
+  res.render('./questionsTest.hbs', ques)
 })
 
 router.post('/:id', (req, res) => {
+  console.log('POST route+ID hit')
   let id = req.params.id
-  let ques = data.Questions[id - 1]
-  let x = req.body.id
-  res.render('./questions.hbs', ques)
+  let answer = req.body.value
+  console.log('answer clicked ' + answer + ' ID of the Q is ' + id)
+  let nextQues = data.Questions[id]
+  res.render('./questionsTest.hbs', nextQues)
 })
 
-router.post('/submit/:id', (req, res) => {
-  console.log('redirecting to results')
-  res.redirect('/friends/:id')
-})
+// // changed id -1 to id
+// router.get('/:id', (req, res) => {
+//   let id = req.params.id
+//   let ques = data.Questions[id -1]
+//   console.log('questions route hit')
+//   console.log(ques)
+//   res.render('./questions.hbs', ques)
+// })
 
-router.get('/11', (req, res) => {
-  console.log('redirecting to results')
-  res.redirect('/friends/results')
-})
+// router.get('/:id', (req, res) => {
+//   console.log('GET route+ID hit')
+//   let id = req.params.id
+//   let nextQues = data.Questions[id -1]
+//   let answer = req.params.value
+//   console.log('answer clicked ' + answer )
+//   console.log('ID is ' + id )
+//   res.render('./questionsTest.hbs', nextQues)
+// })
+
+
+  // router.post('/submit/:id', (req, res) => {
+  //   console.log('redirecting to results')
+  //   res.redirect('/friends/:id')
+  // })
+
 
 
 module.exports = router
+
+//    Saving for later
+//   function proceed (quesNum, ansNum) {
+//     // fs.writeFile('./answers.json', JSON.stringify(document.getElementById('value').innerHTML), 'utf8', function (err) {
+//     //   if (err) {
+//     //     // eslint-disable-next-line no-console
+//     //     return console.log(err)
+//     //   }
+//     // })
+//     var form = document.createElement('form')
+//     form.setAttribute('method', 'post')
+//     form.setAttribute('action', '/friends/' + (quesNum + 1))
+//     form.style.display = 'hidden'
+//     document.body.appendChild(form)
+//     form.submit()
+// }
